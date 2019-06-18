e9aabeef1beeea93b5dfa0e7696da086663b3e53
Administrator <Administrator@DEEP-2019TLGLWI>
2019-06-19 03:09:54 +0800

    增加缓存

--- SpringSecutriy02_shiro/src/main/java/com/shiro/CredentialMatcher.java
+++ SpringSecutriy02_shiro/src/main/java/com/shiro/CredentialMatcher.java
@@ -1,23 +1,50 @@
 package com.shiro;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheManager;
+import org.springframework.stereotype.Component;
 
 /**
  * 
- * 密码验证 自定义类
+ * 密码验证 自定义类 CredentialsMatcher是一个接口，功能就是用来匹配用户登录使用的令牌和数据库中保存的用户信息是否匹配
+ * 
+ * 
  * @author Administrator
  *
  */
+@Component()
 public class CredentialMatcher extends SimpleCredentialsMatcher {
+	// 声明一个缓存接口，这个接口是Shiro缓存管理的一部分，它的具体实现可以通过外部容器注入
+	private Cache<String, AtomicInteger> passwordRetryCache;
 
-    @Override
-    public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
-        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
-        String password = new String(usernamePasswordToken.getPassword());
-        String dbPassword = (String) info.getCredentials();
-        return this.equals(password, dbPassword);
-    }
+	public CredentialMatcher(CacheManager cacheManager) {
+		passwordRetryCache = cacheManager.getCache("passwordRetryCache");
+	}
+
+	@Override
+	public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
+		UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) token;
+		String password = new String(usernamePasswordToken.getPassword());
+		String dbPassword = (String) info.getCredentials();
+		AtomicInteger retryCount = passwordRetryCache.get(usernamePasswordToken.getUsername());
+		if (retryCount == null) {
+			retryCount = new AtomicInteger(0);
+			passwordRetryCache.put(usernamePasswordToken.getUsername(), retryCount);
+		}
+		// 自定义一个验证过程：当用户连续输入密码错误5次以上禁止用户登录一段时间
+		if (retryCount.incrementAndGet() > 5) {
+			throw new RuntimeException();// 抛出异常,禁止登陆,等待缓存自动清除
+		}
+		boolean isEqual = this.equals(password, dbPassword);
+		if (isEqual) {
+			passwordRetryCache.remove(usernamePasswordToken.getUsername());// 清除缓存
+		}
+		return isEqual;
+	}
 }
--- SpringSecutriy02_shiro/src/main/java/com/shiro/ShiroConfiguration.java
+++ SpringSecutriy02_shiro/src/main/java/com/shiro/ShiroConfiguration.java
@@ -3,6 +3,7 @@ package com.shiro;
 import java.util.LinkedHashMap;
 
 import org.apache.shiro.cache.MemoryConstrainedCacheManager;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
@@ -12,73 +13,87 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import net.sf.ehcache.CacheManager;
+
 @Configuration
 public class ShiroConfiguration {
 
-    @Bean("shiroFilter")
-    public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager manager) {
-        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
-        bean.setSecurityManager(manager);
-        // 设定登录页面
-        bean.setLoginUrl("/login");
-        // 登录成功页面
-        bean.setSuccessUrl("/index");
-        // 无权限页面
-        bean.setUnauthorizedUrl("/unauthorized");
-        /*
-         * 该Map 的Key为URL的正则  Value为对应的拦截
-         * 
-         * DefaultFilter可以查看authc  anon等定义
-         */
-        LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
-        // 表单拦截验证
-        filterChainDefinitionMap.put("/index", "authc");
-        // 匿名拦截验证
-        filterChainDefinitionMap.put("/login", "anon");
-        filterChainDefinitionMap.put("/loginUser", "anon");
-        // 角色拦截验证
-        filterChainDefinitionMap.put("/admin", "roles[admin]");
-        // 权限拦截验证
-        filterChainDefinitionMap.put("/edit", "perms[edit]");
-        filterChainDefinitionMap.put("/druid/**", "anon");
-        // 用户拦截验证
-        filterChainDefinitionMap.put("/**", "user");
-        bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+	@Bean("shiroFilter")
+	public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager manager) {
+		ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+		bean.setSecurityManager(manager);
+		// 设定登录页面
+		bean.setLoginUrl("/login");
+		// 登录成功页面
+		bean.setSuccessUrl("/index");
+		// 无权限页面
+		bean.setUnauthorizedUrl("/unauthorized");
+		/*
+		 * 该Map 的Key为URL的正则 Value为对应的拦截
+		 * 
+		 * DefaultFilter可以查看authc anon等定义
+		 */
+		LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+		// 表单拦截验证,authc
+		filterChainDefinitionMap.put("/index", "authc");
+		// 匿名拦截验证
+		filterChainDefinitionMap.put("/login", "anon");
+		filterChainDefinitionMap.put("/loginUser", "anon");
+		// 角色拦截验证
+		filterChainDefinitionMap.put("/admin", "roles[admin]");
+		// 权限拦截验证
+		filterChainDefinitionMap.put("/edit", "perms[edit]");
+		filterChainDefinitionMap.put("/druid/**", "anon");
+		// 用户拦截验证
+		filterChainDefinitionMap.put("/**", "user");
+		bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
 
-        return bean;
-    }
+		return bean;
+	}
 
-    @Bean("securityManager")
-    public SecurityManager securityManager(@Qualifier("authRealm") AuthRealm authRealm) {
-        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
-        manager.setRealm(authRealm);
-        return manager;
-    }
+	@Bean("securityManager")
+	public SecurityManager securityManager(@Qualifier("authRealm") AuthRealm authRealm,EhCacheManager ehCacheManager) {
+		DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+		manager.setRealm(authRealm);
+		// 使用缓存
+		// manager.setCacheManager(ehCacheManager);
+		return manager;
+	}
 
-    @Bean("authRealm")
-    public AuthRealm authRealm(@Qualifier("credentialMatcher") CredentialMatcher matcher) {
-        AuthRealm authRealm = new AuthRealm();
-        authRealm.setCacheManager(new MemoryConstrainedCacheManager());
-        authRealm.setCredentialsMatcher(matcher);
-        return authRealm;
-    }
+	@Bean("authRealm")
+	public AuthRealm authRealm(@Qualifier("credentialMatcher") CredentialMatcher matcher) {
+		AuthRealm authRealm = new AuthRealm();
+		authRealm.setCacheManager(new MemoryConstrainedCacheManager());
+		authRealm.setCredentialsMatcher(matcher);
+		return authRealm;
+	}
 
-    @Bean("credentialMatcher")
-    public CredentialMatcher credentialMatcher() {
-        return new CredentialMatcher();
-    }
+	@Bean("credentialMatcher")
+	public CredentialMatcher credentialMatcher(@Qualifier("ehCacheManager") EhCacheManager ehCacheManager) {
+		return new CredentialMatcher(ehCacheManager);
+	}
 
-    @Bean
-    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(@Qualifier("securityManager") SecurityManager securityManager) {
-        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
-        advisor.setSecurityManager(securityManager);
-        return advisor;
-    }
+	@Bean
+	public EhCacheManager ehCacheManager(CacheManager cacheManager) {
+		EhCacheManager em = new EhCacheManager();
+		// 将ehcacheManager转换成shiro包装后的ehcacheManager对象
+		em.setCacheManager(cacheManager);
+		// em.setCacheManagerConfigFile("classpath:ehcache.xml");
+		return em;
+	}
 
-    @Bean
-    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
-        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
-        creator.setProxyTargetClass(true);
-        return creator;
-    }
+	@Bean
+	public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(
+			@Qualifier("securityManager") SecurityManager securityManager) {
+		AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+		advisor.setSecurityManager(securityManager);
+		return advisor;
+	}
+
+	@Bean
+	public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+		DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
+		creator.setProxyTargetClass(true);
+		return creator;
+	}
 }
